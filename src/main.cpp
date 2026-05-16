55eed07f85fe34072ddfd96f3cab087b5253e6b2
Kairav <kairavdutta@gmail.com>
2026-05-16 16:31:01 +0530

    created basic main.cpp structure

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "../include/game.h"
+
+Game *game = nullptr;
+
+int main(int argc, const char** argv){
+    game = new Game();
+    bool fullscreen = strcmp(argv[1], "1") == 0 ? true : false; 
+    //initialising SDL
+    game->init("Sample", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 800);
+    // main game loop
+    while (game->running()){
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+    game->clean();
+    return 0;
+}

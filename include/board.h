763756938ec5662f19e7a814e15307fe7835fa97
Kairav <kairavdutta@gmail.com>
2026-05-16 16:33:52 +0530

    Made basic Include files for some classes

---
+++ include/board.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include <iostream>
+#include <cerrno>
+#include <string>
+#include <cstring>
+
+class Board{
+public:
+    Board();
+    ~Board();
+
+
+
+};
+
+
+
+#endif
---
+++ include/piece.h
@@ -0,0 +1,22 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include <iostream>
+#include <cerrno>
+#include <string>
+#include <cstring>
+
+class Piece{
+public:
+    Piece();
+    ~Piece();
+
+
+
+};
+
+
+
+#endif
